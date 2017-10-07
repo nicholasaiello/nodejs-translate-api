8ebcef6f8d00410e6a5a9e4f846d38a843110563
Nicholas Aiello <nikko.aiello@gmail.com>
2017-10-07 14:42:52 -0500

    basic input validation, and minor refactoring

--- routes/translate.js
+++ routes/translate.js
@@ -7,28 +7,35 @@ const API_KEY = process.env.GOOGLE_API_KEY;
 const BASE_API_URL = 'https://translation.googleapis.com/language/translate/v2';
 
 
-const makeTranslateRequest = (q, to = 'es', frm = 'en') => {
+const _makeTranslateRequest = (q, to = 'es', frm = 'en') => {
   const url = `${BASE_API_URL}?q=${q}&target=${to}&source=${frm}&key=${API_KEY}`;
 
   console.log('makeTranslateRequest', url);
   return fetch(url, { method: 'GET' })
     .then(response => response.json())
-    .then(json => {
-      return ('error' in json) 
+    .then(json => (
+      ('error' in json) 
         ? json.error 
-        : (json.data.translations || [{translatedText: null}])[0];
-    })
+        : (json.data.translations || [{translatedText: null}])[0]
+    ))
 }
 
 // only for testing
 router.get('/:frm/:to', async (req, res) => {
   const frm = req.params.frm,
     to = req.params.to,
-    text = req.query.text;
+    text = req.query.text || null;
 
-  const data = await makeTranslateRequest(text, to, frm);
-
-  res.send({ result: data, success: data.errors === undefined });
+  if (text && /^[\w\s\-\'\?\.\,]{2,256}$/.test(text)) {
+    const data = await _makeTranslateRequest(text, to, frm);
+    res.send({
+      result: data,
+      success: data.errors === undefined });
+  } else {
+    res.send({
+      result: { message: 'Invalid string provided' },
+      success: false });
+  }
 });
 
 // TODO
